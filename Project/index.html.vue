a97d1a249174a030ac28efae8174b094b23689e0
Judith <chinonsojudith618@gmail.com>
2022-02-14 15:01:14 +0100

    Introduction to git

---
+++ Project/index.html.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="main">
+      <div class="head">
+        <div>
+          <h3>HMS</h3>
+        </div>
+        <nav class="navbar">
+          <ul>
+            <li>Home</li>
+            <li class="inactive">Features</li>
+          </ul>
+        </nav>
+      </div>
+      <!-- Image -->
+      <div class="bg-img">
+        <h2>Explore the world with our great hospitality</h2>
+        <button>Booking</button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped></style>
